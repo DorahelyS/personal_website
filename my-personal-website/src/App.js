30abb0fa2ec24917c8d590f8297d0d8c5f1c6ab1
Dorahely Sanchez <dorahelys@gmail.com>
2024-03-13 12:19:33 -0500

    style updates

--- my-personal-website/src/App.js
+++ my-personal-website/src/App.js
@@ -5,7 +5,7 @@ import MainPage from "./components/MainPage/MainPage";
 function App() {
   return (
     <div className="App">
-        <MainPage></MainPage>
+      <MainPage></MainPage>
     </div>
   );
 }
--- my-personal-website/src/components/MainPage/MainPage.js
+++ my-personal-website/src/components/MainPage/MainPage.js
@@ -1,17 +1,24 @@
 import React from "react";
 
 function MainPage() {
+    const buttonLabels = ["Projects", "About", "Resume", "Education", "Blogs", "Contact"]
     return (
-        <div class="App">
+        <div className="App">
             <div>
-                <header class="bg-orange-50">
-                    <div class="flex justify-left ml-4">
-                        <img class="h-20" src="/pink_linenbg copy.png" alt="Dorahely Logo" />
+                <header className="bg-orange-50">
+                    <div className="flex justify-left ml-4">
+                        <img className="h-20" src="/pink_linenbg copy.png" alt="Dorahely Logo" />
                     </div>
                 </header>
             </div>
-            <div class="border-double border-4 border-rose-100 h-screen mt-2 mb-4 ml-2 mr-2">
-                <img src="/dorahely.jpg" alt="Dorahely Photo" width="400" height="auto"></img>
+            <div className="border-double border-4 border-rose-100 h-screen mt-2 mb-4 ml-2 mr-2 flex justify-left">
+                <img src="/dorahely.jpg" alt="Dorahely Photo" width="auto" height="auto" style={{ marginTop: '30px', marginBottom: '30px', marginLeft: '20px' }}></img>
+                <div>
+                    {/* instead of writing code for each button - too repetitive mapping over just one & only updating the label */}
+                    {buttonLabels.map((label, index) => (
+                        <button key={index} className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded" style={{ marginTop: '30px', marginBottom: '30px', marginLeft: '20px' }}>{label}</button>
+                    ))}
+                </div>
             </div>
         </div>
     );
@@ -21,3 +28,4 @@ export default MainPage;
 
 {/* remember using src attribute for an image - it is relative to the public directory of my project  / means I am starting from the root directory */ }
 {/* // remember . represents current directory - this location & .. representsthe parent directory - one level up */ }
+{/*<button key={index} class="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded" style={{ marginTop: '30px', marginBottom: '30px', marginLeft: '20px' }}>{label}</button> */ }
