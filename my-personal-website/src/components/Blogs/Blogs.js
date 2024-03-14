b7ca1e6bd5521ba98f374e1eb462c016c2df70f9
Dorahely Sanchez <dorahelys@gmail.com>
2024-03-14 15:11:53 -0500

    updated font & added footer

--- my-personal-website/src/components/Blogs/Blogs.js
+++ my-personal-website/src/components/Blogs/Blogs.js
@@ -6,16 +6,20 @@ function Blogs() {
         <div className="App">
             <div>
                 <header className="bg-zinc-50">
-                    <div className="flex justify-left ml-4">
+                    <div className="flex justify-between items-center ml-4">
                         <img className="h-28" src="/pink_linenbg copy.png" alt="Dorahely Logo" />
+
+                        <div className="mr-4">
+                            <NavLink to={'/'} className="bg-rose-500/50 hover:bg-neutral-50 text-black text-sky-600 rounded px-4 py-4" style={{fontFamily: 'Parisienne'}}>home</NavLink>
+                        </div>
                     </div>
                 </header>
             </div>
-
-            <div className="flex items-center justify-center" >
-                <NavLink to={'/'} className="bg-rose-500/50 hover:bg-neutral-50 text-black text-sky-600 rounded px-4 py-4 font-Parisienne" style={{ marginTop: 'auto', marginBottom: 'auto', marginLeft: '24px', marginRight: '18px' }} > home</NavLink>
+            <div className="border-double border-4 rounded-lg border-rose-100 h-screen flex justify-left background-color: bg-neutral-50">
 
             </div>
+
+
         </div>
     );
 }
--- my-personal-website/src/components/MainPage/MainPage.js
+++ my-personal-website/src/components/MainPage/MainPage.js
@@ -16,17 +16,20 @@ function MainPage() {
             </div>
             <div className="border-double border-4 rounded-lg border-rose-100 h-screen flex justify-left background-color: bg-neutral-50">
                 <img src="/dorahely.jpg" alt="Dorahely" width="auto" height="auto" style={{ marginTop: '30px', marginBottom: '30px', marginLeft: '30px' }}></img>
-                <div className="flex items-center justify-center" >
+                <div  className="flex items-center" >
                     {/* instead of writing code for each button - too repetitive mapping over just one & only updating the label */}
                     {/*buttonLabels.map((label, index) => (
                         <button key={index} className="bg-rose-500/50 hover:bg-neutral-50 text-black text-sky-600 rounded px-4 py-4 font-Parisienne" style={{ marginTop: 'auto', marginBottom: 'auto', marginLeft: '24px', marginRight: '18px'}} >{label}</button>
                     ))*/}
-                    <NavLink to={'/about'} className="bg-rose-500/50 hover:bg-neutral-50 text-black text-sky-600 rounded px-4 py-4 font-Parisienne" style={{ marginTop: 'auto', marginBottom: 'auto', marginLeft: '24px', marginRight: '18px'}} > about</NavLink>
-                    <NavLink to={'/projects'} className="bg-rose-500/50 hover:bg-neutral-50 text-black text-sky-600 rounded px-4 py-4 font-Parisienne" style={{ marginTop: 'auto', marginBottom: 'auto', marginLeft: '24px', marginRight: '18px'}} > projects</NavLink>
-                    <NavLink to={'/blogs'} className="bg-rose-500/50 hover:bg-neutral-50 text-black text-sky-600 rounded px-4 py-4 font-Parisienne" style={{ marginTop: 'auto', marginBottom: 'auto', marginLeft: '24px', marginRight: '18px'}} > blogs</NavLink>
-                    <NavLink to={'resume'} className="bg-rose-500/50 hover:bg-neutral-50 text-black text-sky-600 rounded px-4 py-4 font-Parisienne" style={{ marginTop: 'auto', marginBottom: 'auto', marginLeft: '24px', marginRight: '18px'}} > resume</NavLink>
+                    <NavLink to={'/about'} className="bg-rose-500/50 hover:bg-neutral-50 text-black text-sky-600 rounded px-4 py-4" style={{fontFamily: 'Parisienne', marginLeft:'160px' }}> about</NavLink>
+                    <NavLink to={'/projects'} className="bg-rose-500/50 hover:bg-neutral-50 text-black text-sky-600 rounded px-4 py-4 font-Parisienne" style={{fontFamily: 'Parisienne', marginLeft:'60px'}}> projects</NavLink>
+                    <NavLink to={'/blogs'} className="bg-rose-500/50 hover:bg-neutral-50 text-black text-sky-600 rounded px-4 py-4 font-Parisienne" style={{fontFamily: 'Parisienne', marginLeft:'60px' }}> blogs</NavLink>
+                    <NavLink to={'resume'} className="bg-rose-500/50 hover:bg-neutral-50 text-black text-sky-600 rounded px-4 py-4 font-Parisienne" style={{ fontFamily: 'Parisienne', marginLeft:'60px'}}> resume</NavLink>
+            
                 </div>
+              
             </div>
+            <footer className="bg-zinc-50 py-4 text-center " style={{ fontSize: 'small', fontFamily: 'Parisienne'}}>made with React and  ☕</footer>
         </div>
     );
 }
